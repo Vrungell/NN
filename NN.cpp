6c9658c32cb6f547b4d5ae3765c5e72039d6f11c
Vrungell <julia.kaliberda@gmail.com>
2016-11-21 23:43:44 +0700

    NN

---
+++ NN.cpp
@@ -0,0 +1,13 @@
+// NN.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "core.h"
+#include <vector>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    return 0;
+}
+
