f010f318705bdd77066a1953ce851478e92f8df4
Vrungell <julia.kaliberda@gmail.com>
2016-11-21 23:51:32 +0700

    NN

---
+++ Layer.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include <vector>
+#include "Layer.h"
+#include "MLP.h"
+
+Layer::Layer(int number, int previous_number, int layernumber)
+{
+    number_of_layer = layernumber;
+    number_of_neurons = number;
+    number_of_neurons_previous = previous_number;
+    for (int i = 0; i < number; i++)
+        for (int j = 0; j < previous_number; j++)
+        weights[i][j] = rand();
+}
+
+void Layer::MakeInputValue(std::vector<float>value){
+    for (int i = 0; i < input_value.size(); i++)
+        input_value[i] = value[i];
+} 
+
+std::vector<float> Layer::GetInputValue(){
+    return this->input_value;
+}
+
+void Layer::CountingNET(){
+    for (int i = 0; i < number_of_neurons; i++)
+        for (int j = 0; j < number_of_neurons_previous; j++)
+            NET[i] = NET[i] + weights[i][j] * input_value[j];
+}
+
+void Layer::CountingActivation(){
+    for (int i = 0; i < number_of_neurons; i++)
+        neurons[i] = 1 / (1 + exp(alpha*NET[i]));
+}
+
+float Layer::GetErrors(){
+    float sum;
+    for (int i = 0; i < number_of_layer; i++)
+        for (int j = 0; j < number_of_neurons_previous; j++)
+            sum = sum + errors[i] * weights[i][j];
+    return sum;
+}
+
+void Layer::Error(int number_of_layer, MLP mlp){
+    if (number_of_layer == 3) {
+        for (int i = 0; i < number_of_neurons; i++)
+            errors[i] = (expected_value[i] - neurons[i])* neurons[i] * (1 - neurons[i]);
+    }
+    else { 
+        for (int i = 0; i < number_of_neurons; i++)
+            errors[i] = neurons[i] * (1 - neurons[i]) * mlp.GetLayer(3).GetErrors();//что изменить?
+    }
+}
+
+void Layer::CountingDw(){
+    for (int i = 0; i < number_of_neurons; i++)
+        dweights[i] = learning_rate * input_value[i] * errors[i];
+}
+
+void Layer::ChangeWeights(){
+    for (int i = 0; i < number_of_neurons; i++)
+        for (int j = 0; j < number_of_neurons_previous; j++)
+            weights[i][j] = weights[i][j] + alpha*dweights[i];
+}
+
+std::vector<float> Layer::GetNET(){
+    return this->NET;
+}
+
+Layer::~Layer(){}
