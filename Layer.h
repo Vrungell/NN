febe44a90bb05237863c1379297539f7e5d2e78f
Vrungell <julia.kaliberda@gmail.com>
2016-11-21 23:52:45 +0700

    NN

---
+++ Layer.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<vector>
+#include "MLP.h"
+class Layer
+{
+private:
+    int number_of_neurons;
+    int number_of_neurons_previous;
+    int number_of_layer;
+    static float alpha;//for activation
+    static float delta, gamma;//for shake
+    static float learning_rate;
+    std::vector<float>dweights;
+    std::vector<std::vector<float>>weights;
+    std::vector<float>NET;
+    std::vector<float>neurons;//value of activation
+    std::vector<float>errors;
+    std::vector<float>expected_value;
+
+public:
+    Layer(int number, int previous_number, int layernumber);
+    std::vector<float>input_value;
+    void MakeInputValue(std::vector<float>value);
+    void CountingNET();
+    void CountingActivation();
+    void Error(int number_of_layer, MLP mlp);
+    std::vector<float> GetInputValue();
+    std::vector<float> GetNET();
+    float Layer::GetErrors();
+    void Layer::CountingDw();
+    void Layer::ChangeWeights();
+    ~Layer();
+};
+
+
