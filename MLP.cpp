d6a3d48bb217b16bd0317778ad5dfc80fcce5036
Vrungell <julia.kaliberda@gmail.com>
2016-11-22 18:24:38 +0700

    Try

--- MLP.cpp
+++ MLP.cpp
@@ -1,14 +1,28 @@
 #include "stdafx.h"
 #include "MLP.h"
 #include "Layer.h"
-MLP::MLP()
+MLP::MLP(int neurons_in_a_layer[2])
 {
-    Layer layer(900, 0, 1);
-    layers.push_back(layer);
-    Layer layer(30, 900, 2);
-    layers.push_back(layer);
-    Layer layer(10, 30, 3);
-    layers.push_back(layer);
+    Layer layer1(neurons_in_a_layer[0], 0, 1);
+    layers.push_back(layer1);
+    Layer layer2(neurons_in_a_layer[1], neurons_in_a_layer[0], 2);
+    layers.push_back(layer2);
+    Layer layer3(neurons_in_a_layer[2], neurons_in_a_layer[1], 3);
+    layers.push_back(layer3);
+}
+
+void MLP::Start(std::vector<float> image){
+    layers[0].Start(image);
+    layers[1].Start(layers[0].GetNeuronsValue());
+    layers[2].Start(layers[1].GetNeuronsValue());
+}
+
+void MLP::BackPropagation(MLP mlp){
+    for (int i = 2; i >= 0; i--){
+        layers[i].Error(i + 1, &mlp);
+        layers[i].CountingDw();
+        layers[i].ChangeWeights();
+    }
 }
 
 Layer MLP::GetLayer(int layernumber){
@@ -26,7 +40,12 @@ bool MLP::LearningIsOK(){
     else return false;
 }
 
-bool 
-MLP::~MLP()
-{
+void MLP::EndOfEra(int number){
+    if (number == number_of_learning_sets) number_of_eras++;
 }
+
+int MLP::GetEndOfEraTimes(){
+    return this->number_of_eras;
+}
+MLP::~MLP()
+{}
