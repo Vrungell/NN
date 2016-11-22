61629adcf05290ba60e4b46c181e12e0705c1633
Vrungell <julia.kaliberda@gmail.com>
2016-11-23 02:37:49 +0700

    Changes

--- NN.cpp
+++ NN.cpp
@@ -11,14 +11,14 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    int neurons_number[3] = { 2, 10, 1 };
+    int neurons_number[3] = { 1, 10, 1 };
     MLP mlp(neurons_number);
     int j = 0;//counting number of iterations
     while (mlp.LearningIsOK() && mlp.GetEndOfEraTimes()<10) //или эпохи прошли какое-то количество раз
     {
         vector<float>image;
         float value;
-        while (image.size() < 2){
+        while (image.size() < 1){
             cin >> value;
             image.push_back(value);
         }
@@ -26,6 +26,10 @@ int _tmain(int argc, _TCHAR* argv[])
         j++;
         //считываем картинку
         mlp.Start(image);
+        cin >> value;
+        vector<float> Evalue;
+        Evalue.push_back(value);
+        mlp.GetLayer(3).MakeExpectedValue(Evalue);
         mlp.BackPropagation(mlp);
         mlp.CountingGeneralError();
         mlp.EndOfEra(j);
