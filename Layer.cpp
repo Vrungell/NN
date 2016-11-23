83960c43fe1b92d80ff6413935c692d99f0953d1
Vrungell <julia.kaliberda@gmail.com>
2016-11-23 22:43:08 +0700

    Changed

--- Layer.cpp
+++ Layer.cpp
@@ -2,16 +2,15 @@
 #include <vector>
 #include "Layer.h"
 #include "MLP.h"
+#include <cmath>
 
 Layer::Layer(int number, int previous_number, int layernumber)
 {
     number_of_layer = layernumber;
     number_of_neurons = number;
     number_of_neurons_previous = previous_number;
-    errors.resize(number_of_neurons);
-    neurons.resize(number_of_neurons);
     weights.resize(number_of_neurons);
-    dweights.resize(number_of_neurons);
+    NET.resize(number_of_neurons);
     for (int i = 0; i < number; i++)
         weights[i].resize(previous_number);
     for (int i = 0; i < number; i++)
@@ -19,15 +18,15 @@ Layer::Layer(int number, int previous_number, int layernumber)
         weights[i][j] = rand();
 }
 
-void Layer::Start(std::vector<float>image){//ÊÀÊ ÏÅĞÅÄÀÒÜ ÈÇ ÑËÎß Â ÑËÎÉ, ÊÀÊ ÑÂßÇÀÒÜ, ×ÒÎ ÂÛÄÀÂÀÒÜ, ×ÒÎ ÄÀËÜØÅ-ÒÎ, ÎÌÃ
+void Layer::Start(std::vector<float>image){
     MakeInputValue(image);
     CountingNET();
     CountingActivation();
 }
 
 void Layer::MakeInputValue(std::vector<float>value){
-    for (int i = 0; i < input_value.size(); i++)
-        input_value[i] = value[i];
+    for (int i = 0; i < value.size(); i++)
+        input_value.push_back(value[i]);
 } 
 
 std::vector<float> Layer::GetInputValue(){
@@ -42,7 +41,7 @@ void Layer::CountingNET(){
 
 void Layer::CountingActivation(){
     for (int i = 0; i < number_of_neurons; i++)
-        neurons[i] = 1 / (1 + exp(alpha*NET[i]));
+        neurons.push_back(1 / (1 + exp(alpha*NET[i])));
 }
 
 std::vector<float> Layer::GetNeuronsValue(){
@@ -60,27 +59,28 @@ float Layer::GetErrors(){
 void Layer::Error(int number_of_layer, MLP &mlp){
     if (number_of_layer == 3) {
         for (int i = 0; i < number_of_neurons; i++)
-            errors[i] = (expected_value[i] - neurons[i])* neurons[i] * (1 - neurons[i]);
+            errors.push_back((expected_value[i] - neurons[i])* neurons[i] * (1 - neurons[i]));
     }
     else { 
         for (int i = 0; i < number_of_neurons; i++)
-            errors[i] = neurons[i] * (1 - neurons[i]) * mlp.GetLayer(3).GetErrors();//÷òî èçìåíèòü?
+            errors.push_back(neurons[i] * (1 - neurons[i]) * mlp.GetLayer(3).GetErrors());//÷òî èçìåíèòü?
     }
 }
 
 void Layer::MakeExpectedValue(std::vector<float>value){
-    expected_value = value;
+    for (int i = 0; i < value.size(); i++)
+        expected_value.push_back(value[i]);
 }
 
 void Layer::CountingDw(){
     for (int i = 0; i < number_of_neurons; i++)
-        dweights[i] = learning_rate * input_value[i] * errors[i];
+        dweights.push_back(learning_rate * input_value[i] * errors[i]);
 }
 
 void Layer::ChangeWeights(){
     for (int i = 0; i < number_of_neurons; i++)
         for (int j = 0; j < number_of_neurons_previous; j++)
-            weights[i][j] = weights[i][j] + alpha*dweights[i];
+            weights[i][j] =  weights[i][j] + alpha*dweights[i];
 }
 
 std::vector<float> Layer::GetNET(){
